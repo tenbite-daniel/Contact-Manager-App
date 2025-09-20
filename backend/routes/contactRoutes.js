36c996e41dfe4158c6910bf6271480eebfa8c122
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-09-20 18:12:22 +0300

    refactor: stacked operation with same endpoints together

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -8,10 +8,7 @@ const {
 	deleteContact,
 } = require("../controllers/contactController");
 
-router.route("/").get(getAllContact);
-router.route("/").post(createContact);
-router.route("/:id").get(getContact);
-router.route("/:id").put(updateContact);
-router.route("/:id").delete(deleteContact);
+router.route("/").get(getAllContact).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
