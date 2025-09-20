b195068b9737eb833b047f9ab1380882c648a6f1
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-09-20 18:13:08 +0300

    feat: called the error handler

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv').config()
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use('/api/contacts', require('./routes/contactRoutes'))
-
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
-})
+	console.log(`Server running on port: ${port}`);
+});
