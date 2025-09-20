3b8449359b95baf62d38072288f018e637452213
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-09-20 20:16:08 +0300

    feat(auth): implement login functionality with jwt access token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 // @desc Register a user
 // @route POST /api/user/register
@@ -15,7 +16,6 @@ const registerUser = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error("User already registered!");
 	}
-
 	// Has password
 	const hashedPassword = await bcrypt.hash(password, 10);
 	const user = await User.create({
@@ -37,6 +37,30 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/user/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		res.status(400);
+		throw new Error("All fields are mandatory");
+	}
+	const user = await User.findOne({ email });
+	//  Compare password with hashedPassowrd
+	if (user && (await bcrypt.compare(password, user.password))) {
+		const accessToken = jwt.sign(
+			{
+				user: {
+					username: user.username,
+					email: user.email,
+					id: user.id,
+				},
+			},
+			process.env.ACCESS_TOKEN_SECRET,
+			{ expiresIn: `${process.env.TOKEN_EXPIRE}` }
+		);
+		res.status(200).json({ accessToken });
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password!");
+	}
 	res.status(200).json({ message: "Login the user" });
 });
 
